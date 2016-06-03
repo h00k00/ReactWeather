53dbba0ff27f6231f05339c151b4fac956a61f84
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-06-03 20:39:52 +0300

    Rename About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
 
 var About = (props) => {
-    return (
-      <h3>About component</h3>
-    )  
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Testing React with this small app</p>
+    </div>
+  )  
 };
 
 module.exports = About;
