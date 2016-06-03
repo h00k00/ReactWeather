563be3bd17dbe6dd4bf8618bdaa1da7b81a738f1
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-06-03 19:48:56 +0300

    Ready for Heroku part II

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 const PORT = process.env.PORT  || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] == 'http') {
+  if (req.protocol == 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
@@ -16,5 +16,20 @@ app.use(function (req, res, next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-    console.log('Server up and listening to port ' + PORT);
+    console.log('Server is up and listening to port ' + PORT);
 });
+
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
+                                     
+                                                          
+                  
