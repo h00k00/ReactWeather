72bbafc0c45e1fe45c7f04c3aa31822c4df518d2
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-06-06 14:11:30 +0300

    Updated About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Testing React with this small app</p>
+      <p>Tools used:</p>
+      <ul>
+        <li>
+          <a href="https://https://facebook.github.io/react">React</a> - JavaScript framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Data from Open WeatherMap.
+        </li>
+      </ul>
     </div>
   )  
 };
